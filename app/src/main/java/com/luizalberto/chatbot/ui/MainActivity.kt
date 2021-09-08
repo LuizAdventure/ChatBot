90627a66b2fe80511b7c903c519fe6e615d3246e
LuizAdventure <luizsbb@gmail.com>
2021-09-08 09:38:27 -0300

    Em desenvolvimento chatBot

---
+++ app/src/main/java/com/luizalberto/chatbot/ui/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.luizalberto.chatbot.ui
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.luizalberto.chatbot.R
+import com.luizalberto.chatbot.data.Message
+import com.luizalberto.chatbot.utils.Constants.RECEIVE_ID
+import com.luizalberto.chatbot.utils.Constants.SEND_ID
+import com.luizalberto.chatbot.utils.BotResponse
+import com.luizalberto.chatbot.utils.Constants.OPEN_GOOGLE
+import com.luizalberto.chatbot.utils.Constants.OPEN_SEARCH
+import com.luizalberto.chatbot.utils.Time
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+
+    var messagesList = mutableListOf<Message>()
+
+    private lateinit var adapter: MessagingAdapter
+    private val botList = listOf("Luiz", "Sara", "Bryan", "Brandon")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView()
+
+        clickEvents()
+
+        val random = (0..3).random()
+        customBotMessage("Olá! Hoje você está falando com ${botList[random]}, em que posso ajudar?")
+    }
+
+    private fun clickEvents() {
+
+        //Envie uma mensagem
+        btn_send.setOnClickListener {
+            sendMessage()
+        }
+
+        //Role para trás para a posição correta quando o usuário clicar na visualização de texto
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+
+                withContext(Dispatchers.Main) {
+                    rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                }
+            }
+        }
+    }
+
+    private fun recyclerView() {
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        rv_messages.layoutManager = LinearLayoutManager(applicationContext)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //Caso haja mensagens, role para baixo ao reabrir o aplicativo
+        GlobalScope.launch {
+            delay(100)
+            withContext(Dispatchers.Main) {
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+
+    private fun sendMessage() {
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        if (message.isNotEmpty()) {
+            //Adiciona à nossa lista local
+            messagesList.add(Message(message, SEND_ID, timeStamp))
+            et_message.setText("")
+
+            adapter.insertMessage(Message(message, SEND_ID, timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+            botResponse(message)
+        }
+    }
+
+    private fun botResponse(message: String) {
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch {
+            //Atraso de resposta falsa
+            delay(1000)
+
+            withContext(Dispatchers.Main) {
+                //Obtém a resposta
+                val response = BotResponse.basicResponses(message)
+
+                //Adiciona à nossa lista local
+                messagesList.add(Message(response, RECEIVE_ID, timeStamp))
+
+                //Insere nossa mensagem no adaptador
+                adapter.insertMessage(Message(response, RECEIVE_ID, timeStamp))
+
+
+                //Rola-nos para a posição da última mensagem
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+
+                //Inicia o Google
+                when (response) {
+                    OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfterLast("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+
+                }
+            }
+        }
+    }
+
+    private fun customBotMessage(message: String) {
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main) {
+                val timeStamp = Time.timeStamp()
+                messagesList.add(Message(message, RECEIVE_ID, timeStamp))
+                adapter.insertMessage(Message(message, RECEIVE_ID, timeStamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount - 1)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/luizalberto/chatbot/utils/BotResponse.kt
@@ -0,0 +1,132 @@
+package com.luizalberto.chatbot.utils
+
+import com.luizalberto.chatbot.utils.Constants.OPEN_GOOGLE
+import com.luizalberto.chatbot.utils.Constants.OPEN_SEARCH
+import java.sql.Date
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.*
+
+object BotResponse {
+
+    fun basicResponses(_message: String): String {
+
+        val random = (0..2).random()
+        val message = _message.toLowerCase(Locale.ROOT)
+
+        return when {
+
+            //Jogar moeda
+            message.contains("jogar") && message.contains("moeda") -> {
+                val r = (0..1).random()
+                val result = if (r == 0) "cara" else "coroa"
+
+                "Joguei uma moeda e ela caiu $result"
+            }
+
+            //calculos matematicos
+            message.contains("qual é o valor de ") -> {
+                val equation: String? = message.substringAfterLast("qual é o valor de")
+                return try {
+                    val answer = SolveMath.solveMath(equation ?: "0")
+                    "$answer"
+
+                } catch (e: Exception) {
+                    "Desculpe, não consigo resolver isso."
+                }
+            }
+
+
+            message.contains("oi") as Boolean -> {
+                when (random) {
+                    0 -> "Ola!"
+                    1 -> "e ai"
+                    2 -> "como vai você!"
+                    else -> "error"
+                }
+            }
+            message.contains("bom dia") as Boolean-> {
+                when (random) {
+                    0 -> "Bom dia!"
+                    1 -> "Bom dia!"
+                    2 -> "Bom dia!"
+                    else -> "error"
+                }
+            }
+            message.contains("boa tarde") as Boolean-> {
+                when (random) {
+                    0 -> "Boa tarde!"
+                    1 -> "Boa tarde!"
+                    2 -> "Boa tarde!"
+                    else -> "error"
+                }
+            }
+            message.contains("boa noite") as Boolean-> {
+                when (random) {
+                    0 -> "Boa noite!"
+                    1 -> "Boa noite!"
+                    2 -> "Boa noite!"
+                    else -> "error"
+                }
+            }
+
+            message.contains("como você está?") as Boolean-> {
+                when (random) {
+                    0 -> "Estou bem, obrigado!"
+                    1 -> "Estou com fome..."
+                    2 -> "Muito bom! E quanto a você?"
+                    else -> "error"
+                }
+
+            }
+            message.contains("o que você pode fazer") -> {
+                when (random) {
+                    0 -> "Eu posso responder algumas das suas perguntas"
+                    1 -> "Posso te ajudar, com alguma coisa"
+                    2 -> "Posso tentar de ajudar"
+                    else -> "error"
+                }
+            }
+            //Horario
+            message.contains("que horas são") as Boolean && message.contains("?")-> {
+                val timeStamp = Timestamp(System.currentTimeMillis())
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+                val date = sdf.format(Date(timeStamp.time))
+
+                date.toString()
+            }
+
+            //Abrir o google
+            message.contains("abrir") && message.contains("google")-> {
+                OPEN_GOOGLE
+            }
+
+            //Pesquise na internet
+            message.contains("procurar")-> {
+                OPEN_SEARCH
+            }
+
+            //Quando o programa não entende ...
+            else -> {
+                when (random) {
+                    0 -> "Não entendo..."
+                    1 -> "Tente me perguntar algo diferente"
+                    2 -> "Sei lá"
+                    else -> "error"
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
